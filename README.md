97dc0fa823e5cb93cc961f3ee102753be05deae6
FG-telecom <carlos.minho2@hotmail.com>
2025-08-03 23:59:01 -0300

    README.md

---
+++ README.md
@@ -0,0 +1,22 @@
+# Pi-Bound installs and configures PiHole and Unbound to be your network's recursive DNS server.
+
+A simple script that installs PiHole, then automatically installs and configures Unbound to be your pihole's DNS server.
+
+This script supports IPv4 as well as IPv6, you will be prompted during the script for your required configuration.
+
+## What if I already have PiHole installed?
+
+You will be prompted during the script to either install PiHole and Unbound (option 1), or to just install Unbound along with required configuration (option 2).
+
+## How to run
+
+Run the following command on your Pi to download the script:
+
+```
+wget -O pi-bound.sh https://raw.githubusercontent.com/kentishh/pihole_unbound/master/pi-bound.sh
+chmod +x pi-bound.sh
+sudo ./pi-bound.sh
+```
+## Final steps
+
+Some DNS checks are completed after the install which will be printed to the screen depending on if they came back successful or not. As long as these tests come back without any errors, you can then alter your DHCP settings to point to the IP of your PI. Otherwise, point your devices manually to use your Pi as your DNS server.
---
+++ pi-bound.sh
@@ -0,0 +1,108 @@
+#!/bin/bash
+
+# Let's check the script is being run as root
+
+if [[ $EUID -ne 0 ]] ; then
+	echo "This script must be run as root to continue, either sudo this script or run under the root account"
+	exit 1
+fi
+
+
+# This function just checks to see if a command is present. This is used to assume the distro we are running.
+is_command() {
+	local check_command="$1"
+
+	command -v "${check_command}" > /dev/null 2>&1
+}
+
+
+# Main install function, this installs pihole, unbound and wget which we use to get some config files
+pihole_install() {
+	if is_command apt-get ; then
+		tput setaf 2; echo "Running Debian based distro, continuing..."
+		tput setaf 2; echo "PiHole installation beginning..."
+		curl -sSL https://install.pi-hole.net | bash
+	else
+		tput setaf 1; echo "This script has been written to run on Debian based distros. Quiting..."
+		exit 1
+	fi
+}
+
+dns_install() {
+	if is_command apt-get; then
+		# Install unbound 
+		tput setaf 2; echo "Updating repositories..."
+		apt update -y > /dev/null 
+		tput setaf 2; echo "Installing unbound..." 
+	       	apt install unbound -y > /dev/null
+	else
+		tput setaf 1; echo "This script has been written to run on Debian based distros. Quiting..."
+		exit 1
+	fi
+}
+
+configure() {
+
+	# Get root hints file and move into unbound install directory
+	tput setaf 2; echo "Getting root hints..."
+	wget -O /var/lib/unbound/root.hints https://www.internic.net/domain/named.root 
+	
+	# Create a monthly cronjob to get root hints
+	tput setaf 2; echo "Creating cron job to get root hints on a monthly basis..."
+	(crontab -l 2>/dev/null; echo "0 0 1 * * wget -O /var/lib/unbound/root.hints https://www.internic.net/domain/named.root") | crontab -
+
+	# Ask user for ipv4 or ipv6 configuration file for unbound
+	read -p "Do you want unbound to resolve IPv6 addresses? (Y/N)" network
+	if $network -eq "N" ; then
+		wget -O /etc/unbound/unbound.conf.d/pi-hole.conf https://raw.githubusercontent.com/kentishh/pihole_unbound/master/unbound-ipv4 
+	else
+		wget -O /etc/unbound/unbound.conf.d/pi-hole.conf https://raw.githubusercontent.com/kentishh/pihole_unbound/master/unbound-ipv6 
+	fi
+
+	# Start and enable unbound service
+	tput setaf 2; echo "Starting Unbound..."
+	systemctl start unbound
+
+  	tput setaf 2; echo "Enabling Unbound to start at boot..."	
+	systemctl enable unbound
+
+	# Alter pihole DNS options
+	sed -i 's/PIHOLE_DNS_1=.*$/PIHOLE_DNS_1=127.0.0.1#5335/' "/etc/pihole/setupVars.conf"
+	sed -i '/PIHOLE_DNS_2=.*$/d' "/etc/pihole/setupVars.conf"	
+}
+
+dns() {
+
+	# Some variables for testing DNS lookups
+	servfail=$(dig sigfail.verteiltesysteme.net @127.0.0.1 -p 5335 | grep SERVFAIL)
+	noerror=$(dig sigok.verteiltesysteme.net @127.0.0.1 -p 5335 | grep NOERROR)
+
+	if [[ $servfail == *"SERVFAIL"* ]]; then
+		tput setaf 2; echo "First DNS test completed successfully."
+	else
+		tput setaf 1; echo "First DNS query returned unexpected result."
+	fi
+
+	if [[ $noerror == *"NOERROR"* ]]; then
+		tput setaf 2; echo "Second DNS test completed successfully."
+	else
+		tput setaf 1; echo " Second DNS query returned unexpected result."
+	fi
+}
+
+
+echo "This script will install pihole, unbound and automatically configure your pihole DNS configuration to use unbound."
+printf "What would you like to do? (enter a number and press enter) \n1) Install Pihole and unbound along with required configuration.\n2) Install unbound along with required configuration.\n"
+
+read answer
+
+if [ "$answer" == "1" ] ;then
+	pihole_install
+	dns_install
+	configure
+	dns
+else
+	dns_install
+	configure
+	dns
+fi
